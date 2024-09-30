464bb3760bf1efd98480259114bc4b6e0cdef7de
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-29 21:23:23 -0500

    Nav bar done

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,16 +1,18 @@
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/other">Other</RouterLink>
+  <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space x-4">
+    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'Other'}">Other</RouterLink>
+    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
   </nav>
 </template>
 
-<style lang="postcss" scoped>
-nav {
+<!--<style lang="postcss" scoped>
+ nav {
   @apply flex justify-center space-x-4;
 
   & .router-link-active {
     @apply underline underline-offset-4;
   }
 }
-</style>
+</style> -->
