b8a378af08a20a6c5550c39f54023f3657b0b286
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-10-03 22:57:43 -0500

    pagenation working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,9 +2,10 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCards from '@/components/MainCards.vue'
+import MainPagination from '@/components/MainPagination.vue'
 </script>
 
 <template>
-<MainCards>
-</MainCards>
+<MainCards></MainCards>
+<MainPagination></MainPagination>
 </template>
