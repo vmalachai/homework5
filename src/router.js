d8bce15dbef76fa6ef94a7f901b48fea678cff09
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-10-03 23:14:06 -0500

    fixing issues

--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ import { useAuth } from './composables/useAuth'
 const{isAuthenticated} = useAuth() 
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
+  { path: '/homework5/', name: 'Home', component: HomePage },
+  { path: '/homework5/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/homework5/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/homework5/login', name: 'LoginPage', component: LoginPage },
+  { path: '/homework5/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
