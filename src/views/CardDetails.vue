1815245d7d100141c3b09dfbcf495de20b7d8760
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-11-17 23:08:56 -0600

    card details finished

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,8 +1,32 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import { useRoute } from 'vue-router'
+import useAPI from '@/composables/useAPI';
+import { faker } from '@faker-js/faker'
+
+const { fetchEmployee, currentEmployee } = useAPI()
+onMounted(async () => {
+    await fetchEmployee(route.params.id)
+})
+
+onUnmounted(() => {
+    currentEmployee.value = null
+})
+
+
 const route = useRoute()
 </script>
 
 <template>
-    {{ route.params.id }}
+  <!--  {{ route.params.id }} -->
+     <main>
+        <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
+            <h1 class="text-6x1 font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
+            <h1 class="text-3x1 p-5">{{ currentEmployee.title }}</h1>
+            <h1 class="text-2x1 p-5">{{ currentEmployee.userName }} @southtexascollege.edu</h1>
+            <h1 class="text-2x1 p-5">{{ currentEmployee.quote }}</h1>
+
+            <img class="p-8" :src="faker.image.urlLoremFlicker({category: 'cat'})" />
+        </div>
+     </main>
 </template>
