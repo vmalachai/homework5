0bf7d9c5297bbd20d4cdf7832b64573f3b732360
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-11-17 23:22:00 -0600

    input form completed

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import useAPI from '@/comosables/useAPI'
+
+const { instance } = useAPI()
+
+const empFirstName = ref('')
+const empLastName = ref('')
+const empUserName = ref('')
+const empTitle = ref('')
+const empQuote = ref('')
+
+const addEmploye = async () => {
+    const { data } = await instance.post('api/employees/create', {
+        userName: empUserName.value,
+        firstName: empFirstName.value,
+        lastName: empLastName.value,
+        title: empTitle,
+        quote: empQuote
+    })
+//clear all textboxes
+    var inputs = document.querySelectorAll('input')
+    input.forEach(singleInput => singleInput.value='')
+
+}
+
+</script>
 <template>
-    Settings
+    <form class="mx-auto mt-10 flex max-w-mf flex-col gap-4 rounded-md bg-white p-8 shadow-lg" @submit.prevent="addEmploye">
+        <input type="text" placeholder="userName" v-model="empUserName" class="rounded-md px-4 py-2 text-xl ring-1 ring-slate-300" />
+        <input type="text" placeholder="firstName" v-model="empFirstName" class="rounded-md px-4 py-2 text-xl ring-1 ring-slate-300" />
+        <input type="text" placeholder="lastName" v-model="empLastName" class="rounded-md px-4 py-2 text-xl ring-1 ring-slate-300" />
+        <input type="text" placeholder="title" v-model="empTitle" class="rounded-md px-4 py-2 text-xl ring-1 ring-slate-300" />
+        <input type="text" placeholder="quote" v-model="empQuote" class="rounded-md px-4 py-2 text-xl ring-1 ring-slate-300" />
+        <button type="submit" class ="bg-green-500 px-4 py-2">Submit</button>
+    </form>
 </template>
