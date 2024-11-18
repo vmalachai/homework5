0a1192e91db3bc32b72bfaf3f7c3534784c43fc3
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-11-17 22:53:23 -0600

    connect to api is working

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,24 +1,41 @@
 <script setup>
 import { faker } from '@faker-js/faker'
-const firstname = faker.person.firstName()
-const lastname = faker.person.lastName()
-const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname })
-const job_title = faker.person.jobTitle()
-const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase()
+//const firstname = faker.person.firstName()
+//const lastname = faker.person.lastName()
+//const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname })
+//const job_title = faker.person.jobTitle()
+//const bio = faker.lorem.lines(2)
+//const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase()
 
+const props = defineProps({
+    employee: {
+        type: Object,
+        required: true,
+        default: () => {
+            return {
+                _id: "65f1b471c1d4a06a0786c3bc",
+                userName: "jperez",
+                firstName: "Joseph",
+                lastName: "Perez",
+                title: "Computer Science Wizard",
+                quote: "You shall not pass!!",
+                __v: 0,
+            }
+        },
+    },
+})
 
 </script>
 
 <template>
 <!--{{ fullName }} <br /> -->
-<RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+<RouterLink :to="{name: 'CardDetails', params: {id: props.employee._id}}">
  <div class = "border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
     <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl"/>
     <div class="p-4">
-        <p class="mb-2 text 2x1 font-bold">{{ fullname }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+        <p class="mb-2 text 2x1 font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ props.employee.title }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ props.employee.quote }}</p>
     </div>
  </div>
 </RouterLink>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,23 @@
 <script setup>
-//import { ref } from 'vue'
+import { onMounted } from 'vue'
 //const title = ref('Hello There ✋')
-import MainCards from '@/components/MainCards.vue'
-import MainPagination from '@/components/MainPagination.vue'
+import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
+import useAPI from '@/composables/useAPI';
+
+const { getEmployees, loading } = useAPI()
+
+onMounted(async () => {
+    await getEmployees()
+
+})
 </script>
 
 <template>
+
+<p v-if="loading">LOADING...</p>
+<div v-else>
 <MainCards></MainCards>
 <MainPagination></MainPagination>
+</div>
 </template>
